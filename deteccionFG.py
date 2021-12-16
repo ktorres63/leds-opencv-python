094ba65b7c7763af36073d5e520247f7fa1779fc
ktorres63 <ktorresaro@unsa.edu.pe>
2021-12-16 17:40:38 -0500

    creo la funcion para detectar contornos

--- deteccionFG.py
+++ deteccionFG.py
@@ -1,7 +1,26 @@
 import cv2
 import numpy as np
 
-img=cv2.imread('src/figGeometricas.png')
+def getContours(img):
+    contours, hierarchy=cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area > 500:
+            #perimetro=cv2.arcLength(cnt,True) 
+            epsilon=0.01*cv2.arcLength(cnt,True) 
+            approx= cv2.approxPolyDP(cnt,epsilon,True)
+            print(epsilon)
+            print(len(approx))
+
+            cv2.drawContours(imgContours,[approx],-1,(110,0,255),2)
+
+
+
+
+
+
+img=cv2.imread('/src/figGeometricas.png')
 imgContours=img.copy()
 imgGray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur= cv2.GaussianBlur(imgGray,(7,7),1)
