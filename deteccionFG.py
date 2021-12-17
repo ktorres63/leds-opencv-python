652a8e84c3be3b7b3436cead257b6ad4af3cc8d4
ktorres63 <ktorresaro@unsa.edu.pe>
2021-12-16 21:55:38 -0500

    añado la coneccion con arduino y sus instrucciones

--- deteccionFG.py
+++ deteccionFG.py
@@ -1,6 +1,13 @@
 import cv2
+import serial
+import time
 
 cap= cv2.VideoCapture(0)
+
+#coneccion arduino
+ser = serial.Serial('/dev/ttyACM0',9600,timeout=1)
+
+time.sleep(2)
 
 
 def getContours(img):
@@ -20,28 +27,32 @@ def getContours(img):
             
             if len(approx)==3:
                 cv2.putText(frame,'Triangulo',(x,y-5),1,1,(0,0,0),1)
+                ser.write(b't')
             elif len(approx) == 4:
                 aspect_ratio = float(w)/h
                 #print("aspect ratio :",aspect_ratio)
                 if 0.5 < aspect_ratio <1.5:
-                    cv2.putText(frame,"cuadrado",(x,y-5),1,1,(0,0,0),1)
+                    cv2.putText(frame,"Cuadrado",(x,y-5),1,1,(0,0,0),1)
+                    ser.write(b'c')
                 else:
-                    cv2.putText(frame,"rectangulo",(x,y-5),1,1,(0,0,0),1)
-
+                    cv2.putText(frame,"Rectangulo",(x,y-5),1,1,(0,0,0),1)
+                    ser.write(b'r')
+   
             elif len(approx) ==5:
-                cv2.putText(frame,"pentagono",(x,y-5),1,1,(0,0,0),1)
-            
-            elif len(approx) ==6:
-                cv2.putText(frame,"hexagono",(x,y-5),1,1,(0,0,0),1)
+                cv2.putText(frame,"Pentagono",(x,y-5),1,1,(0,0,0),1)
+                ser.write(b'p')
 
-            elif len(approx) ==3:
-                cv2.putText(frame,"triangulo",(x,y-5),1,1,(0,0,0),1)
+            elif len(approx) ==6:
+                cv2.putText(frame,"Hexagono",(x,y-5),1,1,(0,0,0),1)
+                ser.write(b'x')
 
             elif len(approx)>=10:
-                cv2.putText(frame,"circulo",(x,y-5),1,1,(0,0,0),1)
+                cv2.putText(frame,"Circulo",(x,y-5),1,1,(0,0,0),1)
+                ser.write(b'i')
             
             else :
                 cv2.putText(frame,"???",(x,y-5),1,1,(0,0,0),1)
+                ser.write(b'?')
                 
 
             #cv2.drawContours(imgContours,[approx],-1,(110,0,255),2) #dibuja los contornos
@@ -67,5 +78,6 @@ while True:
     if cv2.waitKey(1)==27: #con esc
         break
 
+cap.release()
 cv2.destroyAllWindows()
-
+ser.close()
