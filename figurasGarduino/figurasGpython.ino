fce3c1db1377792bb3e890de4d9b5a9853dd9523
ktorres63 <ktorresaro@unsa.edu.pe>
2021-12-16 21:57:30 -0500

    añado el codigo de arduino

---
+++ figurasGarduino/figurasGpython.ino
@@ -0,0 +1,60 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+int led6 = 7;
+int led7 = 8;
+
+int option;
+void setup() {
+  Serial.begin(9600)
+  pinMode(led1, OUTPUT)
+  pinMode(led2, OUTPUT)
+  pinMode(led3, OUTPUT)
+  pinMode(led4, OUTPUT)
+  pinMode(led5, OUTPUT)
+  pinMode(led6, OUTPUT)
+  pinMode(led7, OUTPUT)
+
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    option = Serial.read();
+    Serial.print(option);
+    if (option == 't') { //triangulo
+      digitalWrite(led1, HIGH);
+    }
+    else if (option == 'c') { //cuadrado
+      digitalWrite(led2, HIGH);
+    }
+    else if (option == 'r') { //rectangulo
+      digitalWrite(led3, HIGH);
+    }
+    else if (option == 'p') { //pentagono
+      digitalWrite(led4, HIGH);
+    }
+    else if (option == 'x') { //hexagono
+      digitalWrite(led5, HIGH);
+    }
+    else if (option == 'i') { //circulo
+      digitalWrite(led6, HIGH);
+    }
+    else if (option == '?') { //desconocido
+      digitalWrite(led7, HIGH);
+    }
+    else {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+      digitalWrite(led6, LOW);
+      digitalWrite(led7, LOW);
+    }
+
+
+  }
+
+}
